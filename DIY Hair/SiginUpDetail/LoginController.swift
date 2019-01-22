c28cd3ad9eb97d4fdd34146660bbc48d6d66cc10
jiangyutong <jiangyutong@jiangyutongdeMacBook-Pro.local>
2019-01-22 04:47:40 -0800

    登录快完善

--- DIY Hair/SiginUpDetail/LoginController.swift
+++ DIY Hair/SiginUpDetail/LoginController.swift
@@ -14,16 +14,19 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var email: UITextField!
     
+    var imagenames=["HeadPortrait1","HeadPortrait2","HeadPortrait3","HeadPortrait4","HeadPortrait5","HeadPortrait6","HeadPortrait7"]
+    
+    
     @IBOutlet weak var tips: UILabel!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var buttonAdress: UIButton!
     //    let button2: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 100, y: 450, width: UIScreen.main.bounds.width - 200, height: 50))
-//        button.addTarget(self, action: #selector(onClickSelectButton), for: .touchUpInside)
-//        button.setTitleColor(UIColor(red: 255/255, green: 51/255, blue: 102/255, alpha: 1), for: .normal)
-//        button.setTitle("选择地址", for: .normal)
-//        return button
-//    }()
+    //        let button = UIButton(frame: CGRect(x: 100, y: 450, width: UIScreen.main.bounds.width - 200, height: 50))
+    //        button.addTarget(self, action: #selector(onClickSelectButton), for: .touchUpInside)
+    //        button.setTitleColor(UIColor(red: 255/255, green: 51/255, blue: 102/255, alpha: 1), for: .normal)
+    //        button.setTitle("选择地址", for: .normal)
+    //        return button
+    //    }()
     var labGroup = CBGroupAndStreamView()
     @IBOutlet weak var backImageView: UIImageView!
     
@@ -31,6 +34,7 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
     var mytype:String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let resetBut = UIButton.init(type: .custom)
         resetBut.setTitle("重置", for: .normal)
         resetBut.frame = CGRect(x: 10, y: 10, width: 40, height: 40)
@@ -73,42 +77,42 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
             self.mytype=String(index)
             print("\(valueStr) index = \(index), groupid = \(groupId)")
         }
-       
+        
         startTimer()
-         buttonAdress.addTarget(self, action: #selector(onClickSelectButton), for: .touchUpInside)
+        buttonAdress.addTarget(self, action: #selector(onClickSelectButton), for: .touchUpInside)
         //button.touchUpInSideEvent()
-       
-//        button.touchUpInSideEvent{ (button) in
-//            print("点击了我")
-//
-//
-//                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
-//                    //                button.isStopLoginingAni = true
-//                    let controller = LoginSuccessController()
-//                    controller.transition.animationType = PQTransitionType(rawValue: self.animationType)!
-//                    self.present(controller, animated: true, completion: nil)
-//                })
-//
-//
-//
-//        }
-//        button.touchUpInSideEvent(shifou: islogin) { (button) in
-//            print("点击了我")
-//
-//
-//                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
-//                    //                button.isStopLoginingAni = true
-//                    let controller = LoginSuccessController()
-//                    controller.transition.animationType = PQTransitionType(rawValue: self.animationType)!
-//                    self.present(controller, animated: true, completion: nil)
-//                })
-//
-//
-//
-//            //模仿登录失败/成功
-//
-//        }
-//
+        
+        //        button.touchUpInSideEvent{ (button) in
+        //            print("点击了我")
+        //
+        //
+        //                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
+        //                    //                button.isStopLoginingAni = true
+        //                    let controller = LoginSuccessController()
+        //                    controller.transition.animationType = PQTransitionType(rawValue: self.animationType)!
+        //                    self.present(controller, animated: true, completion: nil)
+        //                })
+        //
+        //
+        //
+        //        }
+        //        button.touchUpInSideEvent(shifou: islogin) { (button) in
+        //            print("点击了我")
+        //
+        //
+        //                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3, execute: {
+        //                    //                button.isStopLoginingAni = true
+        //                    let controller = LoginSuccessController()
+        //                    controller.transition.animationType = PQTransitionType(rawValue: self.animationType)!
+        //                    self.present(controller, animated: true, completion: nil)
+        //                })
+        //
+        //
+        //
+        //            //模仿登录失败/成功
+        //
+        //        }
+        //
         self.view.addSubview(label)
         self.view.addSubview(buttonAdress)
         
@@ -167,30 +171,35 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
         RunLoop.current.add(timer, forMode: RunLoopMode.commonModes)
         return timer
     }
-
+    
     @IBAction func islogin(_ sender: Any) {
+        let a = Int(arc4random()%7)
+        var img=UIImage(named: self.imagenames[a])
+        let data=UIImagePNGRepresentation(img!)
         if let type=self.mytype,let username=self.username.text,let password=self.password.text,let email=self.email.text,let adress = self.label.text
         {
+            
+            
             if(validateEmail(email: email) == true)
             {
-                  islogin=true
+                islogin=true
             }
             
-          
-//            print(self.mytype)
-//            print(self.username.text)
-//            print(self.password.text)
-//            print(self.email.text)
-//            print(self.label.text)
+            
+            //            print(self.mytype)
+            //            print(self.username.text)
+            //            print(self.password.text)
+            //            print(self.email.text)
+            //            print(self.label.text)
         }
-//        else
-//        {
-//            print(self.mytype)
-//            print(self.username.text)
-//            print(self.password.text)
-//            print(self.email.text)
-//            print(self.label.text)
-//        }
+        //        else
+        //        {
+        //            print(self.mytype)
+        //            print(self.username.text)
+        //            print(self.password.text)
+        //            print(self.email.text)
+        //            print(self.label.text)
+        //        }
         if(islogin==true)
         {
             button.touchUpInSideEvent { (button) in
@@ -205,6 +214,21 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
                     LCUser.logIn(username: self.username.text!, password: self.password.text!) { result in
                         switch result {
                         case .success(let user):
+                            let file=AVFile(data: data!)
+                            let obj=AVObject(className: "myuser")
+                            obj.setObject(file, forKey: "image")
+                            obj.setObject(self.username.text!, forKey: "username")
+                            obj.setObject(self.password.text!, forKey: "password")
+                            obj.setObject(self.label.text!, forKey: "email")
+                            obj.setObject(self.mytype, forKey: "type")
+                            obj.saveInBackground({ (resultbool, error) in
+                                if(resultbool){
+                                    print("cg****************")
+                                }else{
+                                    print("sb****************")
+                                }
+                            })
+                            
                             let currentUser = LCUser.current!
                             
                             currentUser.set("adrees", value:self.label.text!)
@@ -231,7 +255,7 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
                 
                 
             }
-
+            
         }
         else
         {
@@ -248,5 +272,10 @@ class LoginController: BaseViewController,CBGroupAndStreamViewDelegate {
         return emailTest.evaluate(with: email)
         
     }
-   
+    
+    @IBAction func back(_ sender: Any) {
+        print("back******")
+        let controller = StartViewController()
+        self.present(controller, animated: true, completion: nil)
+    }
 }
